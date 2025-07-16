a5f4906d38a0b0526b762c8bd0e2cb1d05143e80
Sanket Gaikwad <sanketg367@gmail.com>
2025-07-16 17:05:33 +0000

    add npm properly , when it gives npm build , install and run dev it shou

--- src/components/admin/index.ts
+++
@@ -1,9 +0,0 @@
-
-export * from './about-form';
-export * from './certifications-list';
-export * from './contact-list';
-export * from './education-list';
-export * from './internships-list';
-export * from './ongoing-projects-list';
-export * from './projects-list';
-export * from './profile-form';
